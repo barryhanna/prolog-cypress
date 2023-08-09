7c875d6994a02566a7e5a013fbbdb574cc8f306e
Barry Hanna <barryahanna@gmail.com>
2023-08-09 09:47:08 +0100

    Initial commit

--- cypress/integration/navigation.spec.ts
+++ cypress/integration/navigation.spec.ts
@@ -1,2 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-function
-describe("Sidebar Navigation", () => {});
+describe("Sidebar Navigation", () => {
+  beforeEach(() => {
+    cy.viewport(1025, 900);
+    cy.visit("http://localhost:3000/dashboard");
+  });
+
+  it("links are working", () => {
+    cy.get("nav").contains("Issues").click();
+  });
+});
